e0c3d198c5b13d673f8ddbd56ac8e0274a727ad4
OksanaA <oksanka23l@gmail.com>
2022-07-30 22:08:19 +0500

    Стабильная версия 2

--- src/directives/tooltip.directive.js
+++ src/directives/tooltip.directive.js
@@ -4,6 +4,7 @@ const tooltip = {
         M.Tooltip.init(el, {html: value})
     },
     unmounted(el) {
+        // eslint-disable-next-line no-undef
         const tooltip = M.Tooltip.getInstance(el)
 
         if (tooltip && tooltip.destroy) {
